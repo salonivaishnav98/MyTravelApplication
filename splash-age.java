0b609c11d7583cda8c120d8793b319f396b8dbf2
Saloni Vaishnav <135298678+salonivaishnav98@users.noreply.github.com>
2023-10-12 22:59:53 +0530

    Create splash-age.java

---
+++ splash-age.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent isplash = new Intent(splashpage.this, MainActivity.class);
+                startActivity(isplash);
+                finish();//pop activity from stack
+            }
+        },2000);
+
+
+    }
+}
