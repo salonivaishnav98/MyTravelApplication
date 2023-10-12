0c2fbf7f4e52eb79291877a5f94abba6b9b1a448
Saloni Vaishnav <135298678+salonivaishnav98@users.noreply.github.com>
2023-10-12 23:01:58 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton experienceMP,events,touristattraction,photosbtn,videobtn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        experienceMP = findViewById(R.id.experienceMP);
+
+}
